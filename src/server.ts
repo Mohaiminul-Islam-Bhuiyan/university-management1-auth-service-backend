753edd96e6d3447f64e95631bcf0e0459ad7a976
Mohaiminul Islam Bhuiyan <rafe.aust@gmail.com>
2023-05-29 08:27:46 +0800

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
-import mongoose from "mongoose";
-import conf from "./config/index";
-import app from "./app";
+import mongoose from 'mongoose'
+import conf from './config/index'
+import app from './app'
 
 async function bootstrap() {
-    try {
-        await mongoose.connect(conf.database_url as string);
-        console.log('data base connection established')
+  try {
+    await mongoose.connect(conf.database_url as string)
+    console.log('data base connection established')
 
-        app.listen(conf.port, () => {
-            console.log(`Example app listening on port ${conf.port}`)
-        })
-    }
-    catch (error) {
-        console.log('failed to connect', error);
-    }
-
+    app.listen(conf.port, () => {
+      console.log(`Example app listening on port ${conf.port}`)
+    })
+  } catch (error) {
+    console.log('failed to connect', error)
+  }
 }
 
-bootstrap();
+bootstrap()
